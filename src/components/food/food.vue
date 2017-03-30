76334f997bf05403d6817d878e697ed0b50e3a68
yuelight <vaeqfe@gmail.com>
2017-03-30 08:11:52 +0800

    rating select

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="food" v-show="showFlag" transition="move">
+    <div class="food" v-show="showFlag" transition="move" v-el:food>
         <div class="food-content">
             <div class="image-header">
                 <img :src="food.image">
@@ -7,11 +7,45 @@
                     <i class="icon-arrow_lift"></i>
                 </div>
             </div>
+            <div class="content">
+                <h1 class="title">{{ food.name }}</h1>
+                <div class="detail">
+                    <span class="sell-count">月售{{ food.sellCount }}份</span>
+                    <span class="rating">好评率{{ food.rating }}%</span>
+                </div>
+                <div class="price">
+                    <span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                    <cartcontrol :food="food"></cartcontrol>
+                </div>
+                <div class="buy" @click.stop.prevent="addFirst($event)" transition="fade" v-show="!food.count || food.count == 0">加入购物车</div>
+            </div>
+            <split v-show="food.info"></split>
+            <div class="info" v-show="food.info">
+                <h1 class="title">商品信息</h1>
+                <p class="text">{{ food.info }}</p>
+            </div>
+            <split v-show="food.info"></split>
+            <div class="rating">
+                <h1 class="title">商品评价</h1>
+                <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+            </div>
         </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Vue from 'vue';
+import BScroll from 'better-scroll';
+import cartcontrol from 'components/cartcontrol/cartcontrol';
+import ratingselect from 'components/ratingselect/ratingselect';
+import split from 'components/split/split';
+
+// const POSITIVE = 0;
+// const NEGATIVE = 1;
+const ALL = 2;
+
 export default {
     props: {
         food: {
@@ -20,16 +54,46 @@ export default {
     },
     data() {
         return {
-            showFlag: false
+            showFlag: false,
+            selectType: ALL,
+            onlyContent: true,
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽'
+            }
         };
     },
     methods: {
         show() {
             this.showFlag = true;
+            this.selectType = ALL;
+            this.onlyContent = false;
+            this.$nextTick(() => {
+                if (!this.scroll) {
+                    this.scroll = new BScroll(this.$els.food, {
+                        click: true
+                    });
+                } else {
+                    this.scroll.refresh();
+                }
+            });
         },
         hide() {
             this.showFlag = false;
+        },
+        addFirst(event) {
+            if (!event._constructed) {
+                return;
+            }
+            this.$dispatch('cart.add', event.target);
+            Vue.set(this.food, 'count', 1);
         }
+    },
+    components: {
+        cartcontrol,
+        ratingselect,
+        split
     }
 };
 </script>
@@ -68,4 +132,74 @@ export default {
                     padding: 10px;
                     font-size: 20px;
                     color: #fff;
+        .content
+            position: relative;
+            padding: 18px;
+            .title
+                line-height: 14px;
+                margin-bottom: 8px;
+                font-size: 14px;
+                font-weight: 700;
+                color: rgb(7,17,27);
+            .detail
+                margin-bottom: 18px;
+                line-height: 10px;
+                height: 10px;
+                font-size: 0;
+                .sell-count, .rating
+                    font-size: 10px;
+                    color: rgb(147,153,159);
+                .sell-count
+                    margin-right: 12px;
+            .price
+                font-weight: 700;
+                line-height: 24px;
+                .now
+                    margin-right: 18px;
+                    font-size: 14px;
+                    color: rgb(240,20,20);
+                .old
+                    text-decoration: line-through;
+                    font-size: 10px;
+                    color: rgb(147,153,159);
+            .cartcontrol-wrapper
+                position: absolute;
+                right: 12px;
+                bottom: 12px;
+            .buy
+                position: absolute;
+                right: 18px;
+                bottom: 18px;
+                z-index: 10;
+                height: 24px;
+                line-height: 24px;
+                padding: 0 12px;
+                box-sizing: border-box;
+                border-radius: 12px;
+                font-size: 10px;
+                background: rgb(0,160,220);
+                &.fade-transition
+                    transition: all .2s;
+                    opacity: 1;
+                &.fade-enter, &.fade-leave
+                    opacity: 0;
+        .info
+            padding: 18px;
+            .title
+                line-height: 14px;
+                margin-bottom: 18px;
+                font-size: 14px;
+                color: rgb(7,17,27);
+            .text
+                line-height: 24px;
+                padding: 0 8px;
+                font-size: 12px;
+                color: rgb(77,85,93);
+        .rating
+            padding-top: 18px;
+            .title
+                line-height: 14px;
+                margin-left: 18px;
+                font-size: 14px;
+                color: rgb(7,17,27);
 </style>
